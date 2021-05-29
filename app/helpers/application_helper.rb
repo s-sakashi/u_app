1e5db516970eaf7ecd5ab290e5bc6421ecebb6a3
SSakashita <suguru.sakashita@gmail.com>
2021-05-29 23:27:42 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    # ページごとの完全なタイトルを返す
+    def full_title(page_title = '')
+        app_name = " - U"
+        main_title = "U - 温泉探し情報サイト"
+        if page_title.empty?
+            main_title
+        else
+            page_title + app_name
+        end
+    end
 end
