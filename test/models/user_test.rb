e2a930d6b8bbda6bde9265ef300f8f9c9b422331
SSakashita <suguru.sakashita@gmail.com>
2021-06-04 06:38:02 +0900

    Add index to users email and set downcase only

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,15 +1,22 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+
   def setup
     @user = User.new(name: "Example", email: "user@gmail.com")
   end
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case " do
+    mixed_case_email = "Foo@Exaba.coM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
 end
