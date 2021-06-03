dbc84f850b2170ac2cba7e9cae4e106ffcb978c9
SSakashita <suguru.sakashita@gmail.com>
2021-06-03 22:48:07 +0900

    Modified test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
     # ページごとの完全なタイトルを返す
     def full_title(page_title = '')
         app_name = " - U"
-        main_title = "U - 温泉探し情報サイト"
+        main_title = "U - 温泉情報サイト"
         if page_title.empty?
             main_title
         else
--- test/controllers/home_pages_controller_test.rb
+++ test/controllers/home_pages_controller_test.rb
@@ -2,13 +2,13 @@ require "test_helper"
 
 class HomePagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_pages_home_url
+    get root_url
     assert_response :success
-    # assert_select "title", "U | 温泉情報サイト"
+    assert_select "title", "U - 温泉情報サイト"
   end
 
   test "should get about" do
-    get home_pages_about_url
+    get about_url
     assert_response :success
   end
 end
