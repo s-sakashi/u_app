f59f14193ebe0778a34207bd226f5aa5d035f8ef
SSakashita <suguru.sakashita@gmail.com>
2021-06-06 22:04:55 +0900

    Add login test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,11 +1,16 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
+  
+  def setup
+    @user = users(:sakashi)
+  end
+
+  test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: {
-      email: "", password: "" }}
+      email: "sakashi@gmail.com", password: "invalid" }}
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
