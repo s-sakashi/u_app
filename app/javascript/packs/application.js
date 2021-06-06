54a27022f92fa10c8b01abc5142e4e36e44860c2
SSakashita <suguru.sakashita@gmail.com>
2021-06-06 21:10:39 +0900

    Setting login session

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("jquery") // 追加
+import "bootstrap" // 追加
 
 Rails.start()
 Turbolinks.start()
