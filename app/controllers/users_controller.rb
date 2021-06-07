0f365043bd293b495f0c2eeb78c076fef1564161
SSakashita <suguru.sakashita@gmail.com>
2021-06-07 23:04:46 +0900

    Add test of edit user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-
+      flash[:success] = "更新完了"
+      redirect_to @user
     else
       render 'edit'
     end
