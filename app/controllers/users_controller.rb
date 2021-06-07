9f72233fc7dde2972209b9d7feaa5a0fc6e81cd7
SSakashita <suguru.sakashita@gmail.com>
2021-06-08 00:01:04 +0900

    Add test to redirect if other user edit or update your profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -45,5 +46,11 @@ class UsersController < ApplicationController
         flash[:danger] = "温泉に入る前に、まずは服を脱いでから（※ログインが必要です）"
         redirect_to login_url
       end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
