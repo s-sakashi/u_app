795edd0f736ca3e4762b7b06c2692675191a2352
SSakashita <suguru.sakashita@gmail.com>
2021-06-08 09:31:07 +0900

    Add test that only admin user can destroy other user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -23,8 +23,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: {
-      name: @user.name, email: @user.email}}
+    patch user_path(@user), params: { user: { name: @user.name,
+      email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
@@ -52,6 +52,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                         password_confirmation: "password",
                         admin: true}}
     assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
   end
 
 end
