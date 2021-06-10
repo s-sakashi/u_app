c53617de918debd767bc973be3507c82ab196e0a
SSakashita <suguru.sakashita@gmail.com>
2021-06-11 07:34:51 +0900

    Add test about account activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,15 @@
 class AccountActivationsController < ApplicationController
+    def edit
+        user = User.find_by(email: params[:email])
+        if user && !user.activated? && user.authenticated?(:activation, params[:id])
+          user.update_attribute(:activated,    true)
+          user.update_attribute(:activated_at, Time.zone.now)
+          log_in user
+          flash[:success] = "アカウントが有効化されました！"
+          redirect_to user
+        else
+          flash[:danger] = "有効化に失敗しました。リンクを確認してください。"
+          redirect_to root_url
+        end
+      end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user &.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1'? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1'? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "アカウントが有効化できませんでした。"
+        message += "もう一度メールのリンクを確認してください。"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "メールアドレス/パスワードが不適切です。"
       render 'new'
