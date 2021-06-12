473ea04a664683d2d967c19ea38f6873a586415a
SSakashita <suguru.sakashita@gmail.com>
2021-06-12 23:19:04 +0900

    Update user mailer previews

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "【U】アカウントの有効化をお願いします"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email , subject: "【U】パスワード再設定"
   end
 end
