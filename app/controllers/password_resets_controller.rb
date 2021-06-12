a77b97557f977f9fee94c8a012fa27f0068cbe97
SSakashita <suguru.sakashita@gmail.com>
2021-06-12 20:36:22 +0900

    Add password reset method in user.rb

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,22 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_passwordreset_email
+      flash[:info] = "パスワード再設定案内メールを送信しました。"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "メールアドレスが存在しません。"
+      render 'new'
+    end
+  end
+
+
   def edit
   end
+
 end
