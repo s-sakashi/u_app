521bfb52bad55ef5e186fcf69c64fc10a7842fa5
SSakashita <suguru.sakashita@gmail.com>
2021-06-13 21:02:44 +0900

    Add hot springs index page

--- app/controllers/hotsprings_controller.rb
+++ app/controllers/hotsprings_controller.rb
@@ -1,5 +1,9 @@
 class HotspringsController < ApplicationController
 
+    def index
+        @hotsprings = Hotspring.paginate(page: params[:page])
+    end
+    
     def show
         @hotspring = Hotspring.find(params[:id])
     end
